35dcecb74c55b6a37b7d27f25bbb11dda5cf2009
jotexdumpe <jotexdumpe@yahoo.com>
2026-01-28 17:35:15 +0100

    Day 5 of learning Django

--- core/main/urls.py
+++ core/main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from . import views
+
+app_name = 'main'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('store/', views.product, name='product'),
+    path('product<int:id>', views.product_details, name='product_details'),
 ]
--- core/main/views.py
+++ core/main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
 
+def product(request):
+    return HttpResponse ("Product Lists")
+
+def product_details(request, id):
+    return HttpResponse (f"Product ID: {id}")
